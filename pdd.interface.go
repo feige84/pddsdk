bc2def3b35fdea96b23a567ae6c21df043f022ab
tanfei84 <feige84@126.com>
2019-08-19 21:40:02 +0800

    xx

--- pdd.interface.go
+++ pdd.interface.go
@@ -217,3 +217,111 @@ func (client *ApiReq) DdkGoodsSearch(keyword string, goodsIds interface{}, sortT
 		return nil, &errInfo
 	}
 }
+
+//pdd.ddk.goods.detail（多多进宝商品详情查询）
+func (client *ApiReq) DdkGoodsDetail(goodsIds int64, pid, customParameters string, zsDuoId, planType int64) (*GoodsDetail, *ApiErrorInfo) {
+	params := ApiParams{}
+	params["goods_id_list"] = "[" + fmt.Sprint(goodsIds) + "]" //商品ID列表。例如：[123456,123]，当入参带有goods_id_list字段，将不会以opt_id、 cat_id、keyword维度筛选商品
+	if pid != "" {
+		params["pid"] = pid //推广位id
+	}
+	if customParameters != "" {
+		params["custom_parameters"] = customParameters //自定义参数
+	}
+	if zsDuoId > 0 {
+		params["zs_duo_id"] = fmt.Sprint(zsDuoId) //招商多多客ID
+	}
+	if planType > 0 {
+		params["plan_type"] = fmt.Sprint(planType) //佣金优惠券对应推广类型，3：专属 4：招商
+	}
+	resp, err := client.Execute("pdd.ddk.goods.detail", params)
+	if err != nil {
+		return nil, err
+	}
+
+	if apiErrInfo := client.CheckApiErr(resp); apiErrInfo != nil {
+		return nil, apiErrInfo
+	}
+
+	goods := &GoodsDetail{}
+
+	goodsResp := resp.Get("goods_detail_response")
+	if goodsResp.Exists() && goodsResp.Get("goods_details").IsArray() {
+		if len(goodsResp.Get("goods_details").Array()) > 0 {
+			detail := goodsResp.Get("goods_details").Array()[0]
+			goods.HasMallCoupon = detail.Get("has_mall_coupon").Bool()
+			goods.MallCouponId = detail.Get("mall_coupon_id").Int()
+			goods.MallCouponDiscountPct = detail.Get("mall_coupon_discount_pct").Int()              //店铺券折扣
+			goods.MallCouponMinOrderAmount = detail.Get("mall_coupon_min_order_amount").Int()       //最小使用金额
+			goods.MallCouponMaxDiscountAmount = detail.Get("mall_coupon_max_discount_amount").Int() //最大使用金额
+			goods.MallCouponTotalQuantity = detail.Get("mall_coupon_total_quantity").Int()          //店铺券总量
+			goods.MallCouponRemainQuantity = detail.Get("mall_coupon_remain_quantity").Int()        //店铺券余量
+			goods.MallCouponStartTime = detail.Get("mall_coupon_start_time").Int()                  //店铺券开始使用时间
+			goods.MallCouponEndTime = detail.Get("mall_coupon_end_time").Int()                      //店铺券结束使用时间
+			goods.CreateAt = detail.Get("create_at").Int()                                          //创建时间（unix时间戳）
+			goods.GoodsId = detail.Get("goods_id").Int()                                            //商品id
+			goods.GoodsName = detail.Get("goods_name").String()                                     //商品名称
+			goods.GoodsDesc = detail.Get("goods_desc").String()                                     //商品描述
+			goods.GoodsThumbnailUrl = detail.Get("goods_thumbnail_url").String()                    //商品缩略图
+			goods.GoodsImageUrl = detail.Get("goods_image_url").String()                            //商品主图
+			if detail.Get("goods_gallery_urls").IsArray() {
+				for _, v := range detail.Get("goods_gallery_urls").Array() {
+					goods.GoodsGalleryUrls = append(goods.GoodsGalleryUrls, v.String()) //商品轮播图
+				}
+			}
+			goods.MinGroupPrice = detail.Get("min_group_price").Int()   //最小拼团价（单位为分）
+			goods.MinNormalPrice = detail.Get("min_normal_price").Int() //最小单买价格（单位为分）
+			goods.MallName = detail.Get("mall_name").String()           //店铺名字
+			goods.MerchantType = detail.Get("merchant_type").Int()      //店铺类型，1-个人，2-企业，3-旗舰店，4-专卖店，5-专营店，6-普通店
+			goods.CategoryId = detail.Get("category_id").Int()          //商品类目ID，使用pdd.goods.cats.get接口获取
+			goods.CategoryName = detail.Get("category_name").String()   //商品类目名
+			goods.OptId = detail.Get("opt_id").Int()                    //商品标签ID，使用pdd.goods.opts.get接口获取
+			goods.OptName = detail.Get("opt_name").String()             //商品标签名
+			if detail.Get("opt_ids").IsArray() {
+				for _, v := range detail.Get("opt_ids").Array() {
+					goods.OptIds = append(goods.OptIds, v.Int()) //商品标签id
+				}
+			}
+			if detail.Get("cat_ids").IsArray() {
+				for _, v := range detail.Get("cat_ids").Array() {
+					goods.CatIds = append(goods.CatIds, v.Int()) //商品类目id
+				}
+			}
+			goods.MallCps = detail.Get("mall_cps").Int()                             //该商品所在店铺是否参与全店推广，0：否，1：是
+			goods.HasCoupon = detail.Get("has_coupon").Bool()                        //商品是否有优惠券 true-有，false-没有
+			goods.CouponMinOrderAmount = detail.Get("coupon_min_order_amount").Int() //优惠券门槛价格，单位为分
+			goods.CouponDiscount = detail.Get("coupon_discount").Int()               //优惠券面额，单位为分
+			goods.CouponTotalQuantity = detail.Get("coupon_total_quantity").Int()    //优惠券总数量
+			goods.CouponRemainQuantity = detail.Get("coupon_remain_quantity").Int()  //优惠券剩余数量
+			goods.CouponStartTime = detail.Get("coupon_start_time").Int()            //优惠券生效时间，UNIX时间戳
+			goods.CouponEndTime = detail.Get("coupon_end_time").Int()                //优惠券失效时间，UNIX时间戳
+			goods.PromotionRate = detail.Get("promotion_rate").Int()                 //佣金比例，千分比
+			goods.GoodsEvalCount = detail.Get("goods_eval_count").Int()              //商品评价数量
+			goods.SalesTip = detail.Get("sales_tip").String()                        //已售卖件数
+			goods.ActivityType = detail.Get("activity_type").Int()                   //活动类型，0-无活动;1-秒杀;3-限量折扣;12-限时折扣;13-大促活动;14-名品折扣;15-品牌清仓;16-食品超市;17-一元幸运团;18-爱逛街;19-时尚穿搭;20-男人帮;21-9块9;22-竞价活动;23-榜单活动;24-幸运半价购;25-定金预售;26-幸运人气购;27-特色主题活动;28-断码清仓;29-一元话费;30-电器城;31-每日好店;32-品牌卡;101-大促搜索池;102-大促品类分会场;
+			if detail.Get("service_tags").IsArray() {
+				for _, v := range detail.Get("service_tags").Array() {
+					goods.ServiceTags = append(goods.ServiceTags, v.Int()) //服务标签: 4-送货入户并安装,5-送货入户,6-电子发票,9-坏果包赔,11-闪电退款,12-24小时发货,13-48小时发货,17-顺丰包邮,18-只换不修,19-全国联保,20-分期付款,24-极速退款,25-品质保障,26-缺重包退,27-当日发货,28-可定制化,29-预约配送,1000001-正品发票,1000002-送货入户并安装
+				}
+			}
+			goods.CltCpnBatchSn = detail.Get("clt_cpn_batch_sn").String()            //店铺收藏券id
+			goods.CltCpnStartTime = detail.Get("clt_cpn_start_time").Int()           //店铺收藏券起始时间
+			goods.CltCpnEndTime = detail.Get("clt_cpn_end_time").Int()               //店铺收藏券截止时间
+			goods.CltCpnQuantity = detail.Get("clt_cpn_quantity").Int()              //店铺收藏券总量
+			goods.CltCpnRemainQuantity = detail.Get("clt_cpn_remain_quantity").Int() //店铺收藏券剩余量
+			goods.CltCpnDiscount = detail.Get("clt_cpn_discount").Int()              //店铺收藏券面额，单位为分
+			goods.CltCpnMinAmt = detail.Get("clt_cpn_min_amt").Int()                 //店铺收藏券使用门槛价格，单位为分
+			goods.DescTxt = detail.Get("desc_txt").String()                          //描述分
+			goods.ServTxt = detail.Get("serv_txt").String()                          //服务分
+			goods.LgstTxt = detail.Get("lgst_txt").String()                          //物流分
+			goods.PlanType = detail.Get("plan_type").Int()                           //推广计划类型 3:定向 4:招商
+			goods.ZsDuoId = detail.Get("zs_duo_id").Int()                            //招商团长id
+		}
+		return goods, nil
+	} else {
+		errInfo := ApiErrorInfo{}
+		errInfo.ErrorCode = 77777
+		errInfo.SubMsg = ApiErrInfo[errInfo.ErrorCode].Error()
+		return nil, &errInfo
+	}
+}
--- pdd.struct.go
+++ pdd.struct.go
@@ -1,61 +1,108 @@
 package pddsdk
 
 type GoodsSearch struct {
-	GoodsList  []GoodsList `json:"goods_list"`
+	GoodsList  []GoodsList `json:"Goods_list"`
 	TotalCount int64       `json:"total_count"`
 	RequestId  string      `json:"request_id"`
 }
 
 type GoodsList struct {
-	HasMallCoupon               bool     `json:"has_mall_coupon"`                 //是否有店铺券
-	MallCouponId                int64    `json:"mall_coupon_id"`                  //店铺券id
-	MallCouponDiscountPct       int64    `json:"mall_coupon_discount_pct"`        //店铺券折扣
-	MallCouponMinOrderAmount    int64    `json:"mall_coupon_min_order_amount"`    //最小使用金额
-	MallCouponMaxDiscountAmount int64    `json:"mall_coupon_max_discount_amount"` //最大使用金额
-	MallCouponTotalQuantity     int64    `json:"mall_coupon_total_quantity"`      //店铺券总量
-	MallCouponRemainQuantity    int64    `json:"mall_coupon_remain_quantity"`     //店铺券余量
-	MallCouponStartTime         int64    `json:"mall_coupon_start_time"`          //店铺券开始使用时间
-	MallCouponEndTime           int64    `json:"mall_coupon_end_time"`            //店铺券结束使用时间
-	CreateAt                    int64    `json:"create_at"`                       //创建时间（unix时间戳）
-	GoodsId                     int64    `json:"goods_id"`                        //商品id
-	GoodsName                   string   `json:"goods_name"`                      //商品名称
-	GoodsDesc                   string   `json:"goods_desc"`                      //商品描述
-	GoodsThumbnailUrl           string   `json:"goods_thumbnail_url"`             //商品缩略图
-	GoodsImageUrl               string   `json:"goods_image_url"`                 //商品主图
-	GoodsGalleryUrls            []string `json:"goods_gallery_urls"`              //商品轮播图
-	MinGroupPrice               int64    `json:"min_group_price"`                 //最小拼团价（单位为分）
-	MinNormalPrice              int64    `json:"min_normal_price"`                //最小单买价格（单位为分）
-	MallName                    string   `json:"mall_name"`                       //店铺名字
-	MerchantType                int64    `json:"merchant_type"`                   //店铺类型，1-个人，2-企业，3-旗舰店，4-专卖店，5-专营店，6-普通店
-	CategoryId                  int64    `json:"category_id"`                     //商品类目ID，使用pdd.goods.cats.get接口获取
-	CategoryName                string   `json:"category_name"`                   //商品类目名
-	OptId                       int64    `json:"opt_id"`                          //商品标签ID，使用pdd.goods.opts.get接口获取
-	OptName                     string   `json:"opt_name"`                        //商品标签名
-	OptIds                      []int64  `json:"opt_ids"`                         //商品标签id
-	CatIds                      []int64  `json:"cat_ids"`                         //商品类目id
-	MallCps                     int64    `json:"mall_cps"`                        //该商品所在店铺是否参与全店推广，0：否，1：是
-	HasCoupon                   bool     `json:"has_coupon"`                      //商品是否有优惠券 true-有，false-没有
-	CouponMinOrderAmount        int64    `json:"coupon_min_order_amount"`         //优惠券门槛价格，单位为分
-	CouponDiscount              int64    `json:"coupon_discount"`                 //优惠券面额，单位为分
-	CouponTotalQuantity         int64    `json:"coupon_total_quantity"`           //优惠券总数量
-	CouponRemainQuantity        int64    `json:"coupon_remain_quantity"`          //优惠券剩余数量
-	CouponStartTime             int64    `json:"coupon_start_time"`               //优惠券生效时间，UNIX时间戳
-	CouponEndTime               int64    `json:"coupon_end_time"`                 //优惠券失效时间，UNIX时间戳
-	PromotionRate               int64    `json:"promotion_rate"`                  //佣金比例，千分比
-	GoodsEvalCount              int64    `json:"goods_eval_count"`                //商品评价数量
-	SalesTip                    string   `json:"sales_tip"`                       //已售卖件数
-	ActivityType                int64    `json:"activity_type"`                   //活动类型，0-无活动;1-秒杀;3-限量折扣;12-限时折扣;13-大促活动;14-名品折扣;15-品牌清仓;16-食品超市;17-一元幸运团;18-爱逛街;19-时尚穿搭;20-男人帮;21-9块9;22-竞价活动;23-榜单活动;24-幸运半价购;25-定金预售;26-幸运人气购;27-特色主题活动;28-断码清仓;29-一元话费;30-电器城;31-每日好店;32-品牌卡;101-大促搜索池;102-大促品类分会场;
-	ServiceTags                 []int64  `json:"service_tags"`                    //服务标签: 4-送货入户并安装,5-送货入户,6-电子发票,9-坏果包赔,11-闪电退款,12-24小时发货,13-48小时发货,17-顺丰包邮,18-只换不修,19-全国联保,20-分期付款,24-极速退款,25-品质保障,26-缺重包退,27-当日发货,28-可定制化,29-预约配送,1000001-正品发票,1000002-送货入户并安装
-	CltCpnBatchSn               string   `json:"clt_cpn_batch_sn"`                //店铺收藏券id
-	CltCpnStartTime             int64    `json:"clt_cpn_start_time"`              //店铺收藏券起始时间
-	CltCpnEndTime               int64    `json:"clt_cpn_end_time"`                //店铺收藏券截止时间
-	CltCpnQuantity              int64    `json:"clt_cpn_quantity"`                //店铺收藏券总量
-	CltCpnRemainQuantity        int64    `json:"clt_cpn_remain_quantity"`         //店铺收藏券剩余量
-	CltCpnDiscount              int64    `json:"clt_cpn_discount"`                //店铺收藏券面额，单位为分
-	CltCpnMinAmt                int64    `json:"clt_cpn_min_amt"`                 //店铺收藏券使用门槛价格，单位为分
-	DescTxt                     string   `json:"desc_txt"`                        //描述分
-	ServTxt                     string   `json:"serv_txt"`                        //服务分
-	LgstTxt                     string   `json:"lgst_txt"`                        //物流分
-	PlanType                    int64    `json:"plan_type"`                       //推广计划类型 3:定向 4:招商
-	ZsDuoId                     int64    `json:"zs_duo_id"`                       //招商团长id
+	HasMallCoupon               bool     `json:"has_mall_coupon"`                //是否有店铺券
+	MallCouponId                int64    `json:"MallCoupon_id"`                  //店铺券id
+	MallCouponDiscountPct       int64    `json:"MallCoupon_discount_pct"`        //店铺券折扣
+	MallCouponMinOrderAmount    int64    `json:"MallCoupon_min_order_amount"`    //最小使用金额
+	MallCouponMaxDiscountAmount int64    `json:"MallCoupon_max_discount_amount"` //最大使用金额
+	MallCouponTotalQuantity     int64    `json:"MallCoupon_total_quantity"`      //店铺券总量
+	MallCouponRemainQuantity    int64    `json:"MallCoupon_remain_quantity"`     //店铺券余量
+	MallCouponStartTime         int64    `json:"MallCoupon_start_time"`          //店铺券开始使用时间
+	MallCouponEndTime           int64    `json:"MallCoupon_end_time"`            //店铺券结束使用时间
+	CreateAt                    int64    `json:"create_at"`                      //创建时间（unix时间戳）
+	GoodsId                     int64    `json:"Goods_id"`                       //商品id
+	GoodsName                   string   `json:"Goods_name"`                     //商品名称
+	GoodsDesc                   string   `json:"Goods_desc"`                     //商品描述
+	GoodsThumbnailUrl           string   `json:"Goods_thumbnail_url"`            //商品缩略图
+	GoodsImageUrl               string   `json:"Goods_image_url"`                //商品主图
+	GoodsGalleryUrls            []string `json:"Goods_gallery_urls"`             //商品轮播图
+	MinGroupPrice               int64    `json:"min_group_price"`                //最小拼团价（单位为分）
+	MinNormalPrice              int64    `json:"min_normal_price"`               //最小单买价格（单位为分）
+	MallName                    string   `json:"mall_name"`                      //店铺名字
+	MerchantType                int64    `json:"merchant_type"`                  //店铺类型，1-个人，2-企业，3-旗舰店，4-专卖店，5-专营店，6-普通店
+	CategoryId                  int64    `json:"category_id"`                    //商品类目ID，使用pdd.goods.cats.get接口获取
+	CategoryName                string   `json:"category_name"`                  //商品类目名
+	OptId                       int64    `json:"opt_id"`                         //商品标签ID，使用pdd.goods.opts.get接口获取
+	OptName                     string   `json:"opt_name"`                       //商品标签名
+	OptIds                      []int64  `json:"opt_ids"`                        //商品标签id
+	CatIds                      []int64  `json:"cat_ids"`                        //商品类目id
+	MallCps                     int64    `json:"mall_cps"`                       //该商品所在店铺是否参与全店推广，0：否，1：是
+	HasCoupon                   bool     `json:"has_coupon"`                     //商品是否有优惠券 true-有，false-没有
+	CouponMinOrderAmount        int64    `json:"coupon_min_order_amount"`        //优惠券门槛价格，单位为分
+	CouponDiscount              int64    `json:"coupon_discount"`                //优惠券面额，单位为分
+	CouponTotalQuantity         int64    `json:"coupon_total_quantity"`          //优惠券总数量
+	CouponRemainQuantity        int64    `json:"coupon_remain_quantity"`         //优惠券剩余数量
+	CouponStartTime             int64    `json:"coupon_start_time"`              //优惠券生效时间，UNIX时间戳
+	CouponEndTime               int64    `json:"coupon_end_time"`                //优惠券失效时间，UNIX时间戳
+	PromotionRate               int64    `json:"promotion_rate"`                 //佣金比例，千分比
+	GoodsEvalCount              int64    `json:"Goods_eval_count"`               //商品评价数量
+	SalesTip                    string   `json:"sales_tip"`                      //已售卖件数
+	ActivityType                int64    `json:"activity_type"`                  //活动类型，0-无活动;1-秒杀;3-限量折扣;12-限时折扣;13-大促活动;14-名品折扣;15-品牌清仓;16-食品超市;17-一元幸运团;18-爱逛街;19-时尚穿搭;20-男人帮;21-9块9;22-竞价活动;23-榜单活动;24-幸运半价购;25-定金预售;26-幸运人气购;27-特色主题活动;28-断码清仓;29-一元话费;30-电器城;31-每日好店;32-品牌卡;101-大促搜索池;102-大促品类分会场;
+	ServiceTags                 []int64  `json:"service_tags"`                   //服务标签: 4-送货入户并安装,5-送货入户,6-电子发票,9-坏果包赔,11-闪电退款,12-24小时发货,13-48小时发货,17-顺丰包邮,18-只换不修,19-全国联保,20-分期付款,24-极速退款,25-品质保障,26-缺重包退,27-当日发货,28-可定制化,29-预约配送,1000001-正品发票,1000002-送货入户并安装
+	CltCpnBatchSn               string   `json:"CltCpn_batch_sn"`                //店铺收藏券id
+	CltCpnStartTime             int64    `json:"CltCpn_start_time"`              //店铺收藏券起始时间
+	CltCpnEndTime               int64    `json:"CltCpn_end_time"`                //店铺收藏券截止时间
+	CltCpnQuantity              int64    `json:"CltCpn_quantity"`                //店铺收藏券总量
+	CltCpnRemainQuantity        int64    `json:"CltCpn_remain_quantity"`         //店铺收藏券剩余量
+	CltCpnDiscount              int64    `json:"CltCpn_discount"`                //店铺收藏券面额，单位为分
+	CltCpnMinAmt                int64    `json:"CltCpn_min_amt"`                 //店铺收藏券使用门槛价格，单位为分
+	DescTxt                     string   `json:"desc_txt"`                       //描述分
+	ServTxt                     string   `json:"serv_txt"`                       //服务分
+	LgstTxt                     string   `json:"lgst_txt"`                       //物流分
+	PlanType                    int64    `json:"plan_type"`                      //推广计划类型 3:定向 4:招商
+	ZsDuoId                     int64    `json:"zs_duo_id"`                      //招商团长id
+}
+
+type GoodsDetail struct {
+	MallCouponId                int64    //店铺优惠券id
+	MallCouponDiscountPct       int64    //店铺折扣
+	MallCouponMinOrderAmount    int64    //最小使用金额
+	MallCouponMaxDiscountAmount int64    //最大使用金额
+	MallCouponTotalQuantity     int64    //店铺券总量
+	MallCouponRemainQuantity    int64    //店铺券余量
+	MallCouponStartTime         int64    //店铺券使用开始时间
+	MallCouponEndTime           int64    //店铺券使用结束时间
+	GoodsId                     int64    //参与多多进宝的商品ID
+	GoodsName                   string   //参与多多进宝的商品标题
+	GoodsDesc                   string   //参与多多进宝的商品描述
+	GoodsImageUrl               string   //多多进宝商品主图
+	GoodsGalleryUrls            []string //商品轮播图
+	MinGroupPrice               int64    //最低价sku的拼团价，单位为分
+	MinNormalPrice              int64    //最低价sku的单买价，单位为分
+	MallName                    string   //店铺名称
+	OptId                       int64    //商品标签ID，使用pdd.goods.opt.get接口获取
+	OptName                     string   //商品标签名称
+	OptIds                      []int64  //商品标签ID
+	CatIds                      []int64  //商品一~四级类目ID列表
+	CouponMinOrderAmount        int64    //优惠券门槛金额，单位为分
+	CouponDiscount              int64    //优惠券面额，单位为分
+	CouponTotalQuantity         int64    //优惠券总数量
+	CouponRemainQuantity        int64    //优惠券剩余数量
+	CouponStartTime             int64    //优惠券生效时间，UNIX时间戳
+	CouponEndTime               int64    //优惠券失效时间，UNIX时间戳
+	PromotionRate               int64    //佣金比例，千分比
+	GoodsEvalCount              int64    //商品评价数
+	CatId                       int64    //商品类目ID，使用pdd.goods.cats.get接口获取
+	SalesTip                    string   //已售卖件数
+	MallId                      int64    //商家id
+	ServiceTags                 []int64  //服务标签: 4-送货入户并安装,5-送货入户,6-电子发票,9-坏果包赔,11-闪电退款,12-24小时发货,13-48小时发货,17-顺丰包邮,18-只换不修,19-全国联保,20-分期付款,24-极速退款,25-品质保障,26-缺重包退,27-当日发货,28-可定制化,29-预约配送,1000001-正品发票,1000002-送货入户并安装
+	CltCpnBatchSn               string   //店铺收藏券id
+	CltCpnStartTime             int64    //店铺收藏券起始时间
+	CltCpnEndTime               int64    //店铺收藏券截止时间
+	CltCpnQuantity              int64    //店铺收藏券总量
+	CltCpnRemainQuantity        int64    //店铺收藏券剩余量
+	CltCpnDiscount              int64    //店铺收藏券面额，单位为分
+	CltCpnMinAmt                int64    //店铺收藏券使用门槛价格，单位为分
+	DescTxt                     string   //描述分
+	ServTxt                     string   //服务分
+	LgstTxt                     string   //物流分
+	PlanType                    int64    //推广计划类型
+	ZsDuoId                     int64    //招商团长id
 }
