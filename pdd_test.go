afdd502efb6003ed27ea04f9a219beee8b247e1c
tanfei84 <feige84@126.com>
2019-09-23 17:22:17 +0800

    呵呵

--- pdd_test.go
+++ pdd_test.go
@@ -13,7 +13,6 @@ func TestExecute(t *testing.T) {
 	//client.WriteErrLog = InsertErrLog
 	client.CacheLife = 0
 	client.AccessToken = ""
-
 	data3, apiErr3 := client.DdkGoodsSearch("纯棉男女宝宝裤子春秋冬季新生儿幼童装外出婴儿衣服大PP加绒长裤", nil, "", "", 1, 1, 10, 0, 0, 0, false, false)
 	jsonData3, jsonErr3 := json.Marshal(data3)
 	fmt.Println("jsonData:", string(jsonData3), jsonErr3, apiErr3)
